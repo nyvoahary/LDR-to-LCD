3bfae2045ae6821f1f0aad4ab6619bde6095b24d
Tolotra <nekenanyvoahary@gmail.com>
2022-02-14 11:21:55 +0300

    Final version

--- LDR to LCD.ino
+++ LDR to LCD.ino
@@ -1,42 +1,38 @@
+//Importing liquidCrystal I2C library
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x3f,16,2);
 
-#define LDR A0
-#define LAMP1 2
-#define LAMP2 3
+// Connect the A0 pin of the LDR module to the A0 pin of Arduino
+// Connect LCD SDA -> A4 and SCL -> A5
 
-/*
-  Behavior,
-  If the value is HIGH, the light in the room should be darker.
-  Otherwise, the light in the room should be lighter.
-*/
-
+#define LDR A0    //Define A0 as LDR 
 
 void setup(){
-  // pinMode(LAMP1,OUTPUT);
-  // pinMode(LAMP2,OUTPUT);
   
+  // Initalize the lcd
   lcd.init();
   lcd.backlight();
-  
-  Serial.begin(9600);
+
 }
 
 void loop() {
   unsigned int AnalogValue;
+  // Read the value from LDR
   AnalogValue = analogRead(LDR);
+
   lcd.setCursor(0,0);
   lcd.print("Brightness:");
   lcd.setCursor(12,0);
   lcd.print(AnalogValue);
   if (AnalogValue >= 680) {
     lcd.setCursor(0,1);
-    lcd.print("Jiro: Maty");    
+    lcd.print("Light: off");
   } else {
       lcd.setCursor(0,1);
-      lcd.print("Jiro: Mirehitra");
+      lcd.print("Light: on");
   }
   delay(1000);
-  lcd.clear();
+
+  lcd.clear(); // Clear LCD to avoid overlay
   
 }
